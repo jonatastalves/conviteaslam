bcf545e6d6ec9f1062e77c7c493e47dfe2633199
jonatas.t.alves <jonatas.t.alves@gmail.com>
2024-07-03 14:51:43 -0300

    salao

--- src/Pages/Principal.jsx
+++ src/Pages/Principal.jsx
@@ -18,20 +18,28 @@ const Principal = () => {
     case '/conviteaslam/festa':
       content = (
         <div>
-          <p>Olá, {userName || 'querido(a) convidado(a)'}!</p>
+          <p>Olá, <strong>{userName || 'querido(a) convidado(a)'}!</strong></p>
           <p>
             Você está convidado(a) para uma aventura incrível no <span className="highlight">Aranhaverso do Aslam</span>! Estamos celebrando o 3º ano do nosso pequeno herói, Aslam, e seria uma honra ter você conosco nesse dia especial.
           </p>
-          <p>Data: 10 de agosto de 2024</p>
-          <p>Horário: 17h</p>
+          <strong><p>Data: 10 de agosto de 2024</p></strong>
+          <strong><p>Horário: 17h</p></strong>
           <p>Venha preparado(a) para um dia cheio de diversão, alegria e muitas surpresas! Esperamos encontrar você lá para tornar este dia ainda mais memorável.</p>
+          <p>Confirme sua presença lá no botãozinho na página inicial!</p>
           <p>Com carinho,</p>
           <p>Papai e Mamãe aranha! 🕸<span className="highlight">❤</span>🕷</p>
         </div>
       );
       break;
     case '/conviteaslam/local':
-      content = <div>Conteúdo de Localização</div>;
+      content = (
+        <div>
+          <p>A Festa será realizada no salão:</p>
+          <h1>Brucy's Festas e Eventos Almeidenses</h1>
+          <p>Localizado na Rua do Queimado, 289 - Bento Ribeiro - RJ</p>
+          <img src="../assets/salao.jpg" alt="" />
+        </div>
+      );        
       break;
     case '/conviteaslam/presentes':
       content = <div>Conteúdo de Sugestão de Presentes</div>;
